e263f987672f0bc87a08151b5d5e64ec953c97b2
Shawkakessy <142739905+ShawkaKessy@users.noreply.github.com>
2024-11-24 00:30:51 +0300

    Update SneakerFilter.java

--- SneakerFilter.java
+++ SneakerFilter.java
@@ -3,9 +3,10 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class SneakerFilter {
-    public static List<Sneaker> filter(List<product> sneakers, Predicate<Sneaker> condition) {
-        return sneakers.stream()
+    public static List<Product> filter(List<Product> products, Predicate<Product> condition) {
+        return products.stream()
                 .filter(condition)
                 .collect(Collectors.toList());
     }
 }
+
